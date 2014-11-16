f6b6140d9e82c0740f3d3dbed46d38e308705c4e
michalizr1 <m.izraitel@gmail.com>
2014-11-16 17:41:25 +0200

    Second Commit

--- src/com/mta/javacourse/michal/izraitel/Java_ProjectServlet.java
+++ src/com/mta/javacourse/michal/izraitel/Java_ProjectServlet.java
@@ -10,5 +10,17 @@ public class Java_ProjectServlet extends HttpServlet {
 		String resultStr = new String("<h1>Result of ("+num2+" + "+num3+") * " +num1+ " + ("+num2+ " * " +num2+ ") = " +res+"</h1>");
 		resp.setContentType("text/html");
 		resp.getWriter().println(resultStr);
-	}
+		
+		int radius = 50;
+		String line1 = new String("<h1>Calculation 1: Area of circle with radius " +radius+ " cm is: " +(int)(Math.PI * Math.pow(radius, 2))+ " square cm.");
+		
+		int angleB = 30, hypotenuse = 50;
+		double radians = Math.toRadians(angleB);
+		double sin = Math.sin(radians);
+		String line2 = new String("<h1>Calculation 2: Length of opposite where angle B is " +angleB+ " degrees and Hypotenuse length is " +hypotenuse+ " cm is: " +(int)(sin * hypotenuse)+ " cm.");
+		
+		int base = 20, exp = 13;
+		String line3 = new String("<h1>Calculation 3: Power of 20 with exp of 13 is: " +(int) Math.pow(base, exp));
+		resp.getWriter().println("<br>" +line1+ "<br>" + line2 + "<br>" + line3);
+		}
 }
